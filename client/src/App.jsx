b1a86b9048b59a1a7f14175dcb35f1da76fcf2e0
Cameron Colaco <wearenotgood@gmail.com>
2022-02-04 10:41:30 -0800

    Added icons package from material UI to package.json dependencies; playing around with MUI in App component; will undo changes before any feature work or pushing to origin.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,28 @@
 import React from 'react';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-
+      title: 'React Working!'
     };
   }
 
   render() {
+    const { title } = this.state;
     return (
       <div>
-        React working!
+        <Typography variant="h1" color="powderblue" align="center">
+          {title}
+        </Typography>
+        <Box display="flex" justifyContent="center">
+          <Button variant="contained" color="primary" onClick={() => console.log('Click!')}>
+            Click Me!
+          </Button>
+        </Box>
       </div>
     );
   }
