042cc2988a4b46d959743473eca6686f219234b8
Cameron Colaco <wearenotgood@gmail.com>
2022-02-05 09:59:26 -0800

    Tests passing; junit.xml updated; will setup react-router-dom for web apps.

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,12 +1,26 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import React from 'react';
 import App from '../client/src/App';
-// import '@testing-library/jest-dom';
+import '@testing-library/jest-dom';
 // import '@testing-library/jest-dom/extend-expect';
 
 describe('<App />', () => {
   test('Renders App component', () => {
     const component = render(<App />);
     expect(component).toBeDefined();
+  });
+  test('Renders title', () => {
+    render(<App />);
+    const appTitle = screen.getByRole('heading', {
+      name: 'React Working!'
+    });
+    expect(appTitle).toHaveTextContent('React Working!');
+  });
+  test('Renders clickable button', () => {
+    render(<App />);
+    const button = screen.getByRole('button', {
+      name: 'Click Me!'
+    });
+    expect(button).toHaveTextContent('Click Me!');
   });
 });
