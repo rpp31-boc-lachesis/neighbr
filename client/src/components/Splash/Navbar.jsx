00dd5c1553f30cad13a3f87bab099ce2b1beadc2
Yitong Liu <yitong414@gmail.com>
2022-02-09 22:19:35 -0500

    Add css for Splash Page text

--- client/src/components/Splash/Navbar.jsx
+++ client/src/components/Splash/Navbar.jsx
@@ -18,9 +18,9 @@ const theme = createTheme({
 const Navbar = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Stack direction="row" spacing={3} justifyContent="end" sx={{ mr: 5, mt: 5 }} >
-            <Button color = 'primary' variant="outlined"><Link to='/login'>Log In</Link></Button>
-            <Button color = 'secondary' variant="outlined"><Link to='/signup'>Sign Up</Link></Button>
+      <Stack direction="row" spacing={3} justifyContent="end" sx={{ mr: 10, mt: 6 }} >
+            <Button size ='large' color = 'primary' variant="outlined"><Link to='/login'>Log In</Link></Button>
+            <Button size ='large' color = 'secondary' variant="outlined"><Link to='/signup'>Sign Up</Link></Button>
       </Stack>
     </ThemeProvider>
 
