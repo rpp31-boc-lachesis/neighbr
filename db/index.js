5c892ac1f21aa1794c67a73ed84af79237f90a61
Cameron Colaco <wearenotgood@gmail.com>
2022-02-11 15:18:02 -0800

    Commit after master merge

--- db/index.js
+++ db/index.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 async function main() {
   try {
     await mongoose.connect('mongodb://localhost:27017/neighbr');
+    // eslint-disable-next-line no-console
     console.log('Mongoose connected to Neighbr!');
   } catch (err) {
+    // eslint-disable-next-line no-console
     console.log(err);
   }
 }
