4c3a5ae40ce74cbc6b1d6f2fc9315be1a959b6ef
Maxine <maxine.sejin.seo@gmail.com>
2022-02-12 10:20:10 -0800

    Removed header component locally

--- client/src/components/Home/Main.jsx
+++ client/src/components/Home/Main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Picker from './Picker.jsx';
 
 export default function Main() {
   return (
     <>
-      <Header />
       <Picker />
       <Footer />
     </>
