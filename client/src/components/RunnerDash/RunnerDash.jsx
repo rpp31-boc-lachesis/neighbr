5aac3ac561c655656bc0e724cf89369a56cebd27
jc <jake.caughern@gmail.com>
2022-02-12 13:16:23 -0600

    fixed button problem

--- client/src/components/RunnerDash/RunnerDash.jsx
+++ client/src/components/RunnerDash/RunnerDash.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 import { styled } from '@mui/material/styles';
 import Box from '@mui/material/Box';
