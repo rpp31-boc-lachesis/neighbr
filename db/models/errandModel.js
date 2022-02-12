080ee59b28b13985b7ec797f66f32a59e5e3e609
Cameron Colaco <wearenotgood@gmail.com>
2022-02-12 13:21:39 -0800

    Added collection object as second argument to schema so that model always references/indexes correct collection in DB.

--- db/models/errandModel.js
+++ db/models/errandModel.js
@@ -1,38 +1,41 @@
 const mongoose = require('mongoose');
 
-const errandSchema = mongoose.Schema({
-  category: String,
-  req_items: {
-    item: String,
-    quantity: Number,
-    weight: String,
-    size: String,
-    notes: String,
-    status: String,
-    requester: String,
-    runner: String,
-    transportation: String
+const errandSchema = mongoose.Schema(
+  {
+    category: String,
+    req_items: {
+      item: String,
+      quantity: Number,
+      weight: String,
+      size: String,
+      notes: String,
+      status: String,
+      requester: String,
+      runner: String,
+      transportation: String
+    },
+    message: {
+      requester: String,
+      notes: String
+    },
+    pickup: {
+      store: String,
+      address: String
+    },
+    dropoff: {
+      requester: String,
+      address: String
+    },
+    date: Date,
+    start_time: Date,
+    end_time: Date,
+    received_rating: {
+      requester: String,
+      rating: Number
+    }
   },
-  message: {
-    requester: String,
-    notes: String
-  },
-  pickup: {
-    store: String,
-    address: String
-  },
-  dropoff: {
-    requester: String,
-    address: String
-  },
-  date: Date,
-  start_time: Date,
-  end_time: Date,
-  received_rating: {
-    requester: String,
-    rating: Number
-  }
-});
+  { collection: 'errands' }
+);
 
 const Errands = mongoose.model('Errand', errandSchema);
 module.exports = Errands;
--- db/models/userModel.js
+++ db/models/userModel.js
@@ -1,115 +1,118 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
-  id: {
-    type: Number,
-    index: true,
-    unique: true,
-    required: true
-  },
-  first_name: {
-    type: String,
-    index: true,
-    required: true
-  },
-  last_name: {
-    type: String,
-    index: true,
-    required: true
-  },
-  username: {
-    type: String,
-    index: true,
-    unique: true,
-    required: true
-  },
-  email: {
-    type: String,
-    index: true,
-    unique: true,
-    required: true
-  },
-  password: {
-    type: String,
-    index: true,
-    required: true
-  },
-  street_address: {
-    type: String,
-    index: true,
-    required: true
-  },
-  city: {
-    type: String,
-    index: true,
-    required: true
-  },
-  state: {
-    type: String,
-    index: true,
-    required: true
-  },
-  zip: {
-    type: String,
-    index: true,
-    required: true
-  },
-  bio: {
-    type: String,
-    required: true
-  },
-  rating: Number,
-  req_history: [
-    {
-      category: String,
-      item: String,
-      quantity: Number,
-      weight: String,
-      size: String,
-      notes: String,
-      status: String,
-      runner: String,
-      date: Date,
-      pickup: {
-        coordinates: {
-          lat: String,
-          lng: String
-        }
-      },
-      dropoff: {
-        coordinates: {
-          lat: String,
-          lng: String
-        }
-      }
-    }
-  ],
-  run_history: [
-    {
-      category: String,
-      item: String,
-      quantity: Number,
-      weight: String,
-      size: String,
-      notes: String,
-      status: String,
-      runner: String,
-      date: Date,
-      pickup: {
-        coordinates: {
-          lat: String,
-          lng: String
-        }
-      },
-      dropoff: {
-        coordinates: {
-          lat: String,
-          lng: String
+const userSchema = mongoose.Schema(
+  {
+    id: {
+      type: Number,
+      index: true,
+      unique: true,
+      required: true
+    },
+    first_name: {
+      type: String,
+      index: true,
+      required: true
+    },
+    last_name: {
+      type: String,
+      index: true,
+      required: true
+    },
+    username: {
+      type: String,
+      index: true,
+      unique: true,
+      required: true
+    },
+    email: {
+      type: String,
+      index: true,
+      unique: true,
+      required: true
+    },
+    password: {
+      type: String,
+      index: true,
+      required: true
+    },
+    street_address: {
+      type: String,
+      index: true,
+      required: true
+    },
+    city: {
+      type: String,
+      index: true,
+      required: true
+    },
+    state: {
+      type: String,
+      index: true,
+      required: true
+    },
+    zip: {
+      type: String,
+      index: true,
+      required: true
+    },
+    bio: {
+      type: String,
+      required: true
+    },
+    rating: Number,
+    req_history: [
+      {
+        category: String,
+        item: String,
+        quantity: Number,
+        weight: String,
+        size: String,
+        notes: String,
+        status: String,
+        runner: String,
+        date: Date,
+        pickup: {
+          coordinates: {
+            lat: String,
+            lng: String
+          }
+        },
+        dropoff: {
+          coordinates: {
+            lat: String,
+            lng: String
+          }
         }
       }
-    }
-  ]
-});
+    ],
+    run_history: [
+      {
+        category: String,
+        item: String,
+        quantity: Number,
+        weight: String,
+        size: String,
+        notes: String,
+        status: String,
+        runner: String,
+        date: Date,
+        pickup: {
+          coordinates: {
+            lat: String,
+            lng: String
+          }
+        },
+        dropoff: {
+          coordinates: {
+            lat: String,
+            lng: String
+          }
+        }
+      }
+    ]
+  },
+  { collection: 'users' }
+);
 
 const Users = mongoose.model('User', userSchema);
 // const someUsername = 'Tom';
