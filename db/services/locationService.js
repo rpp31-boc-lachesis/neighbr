c0d60edcf580f8902ba1f9bda64aa78dec85ca78
jc <jake.caughern@gmail.com>
2022-02-14 19:45:02 -0600

    models and services complete for runs and locations

--- db/services/locationService.js
+++ db/services/locationService.js
@@ -1,14 +1,38 @@
-const Run = require('../models/runs.js');
+const Location = require('../models/locations.js');
 
-const getAllRuns = (callback) => {
-  Run.find().populate
+const createLocation = (locationObject, callback) => {
+  Location.create(locationObject)
     .then((result) => { callback(null, result); })
     .catch((err) => { callback(err, null); });
-}
+};
 
-const getRun = (filter, callback) => {
-  Run.find(filter)
-    .populate('acceptedErrands')
+const getAllLocations = (callback) => {
+  Location.find()
+    .populate({ path: 'runs' })
+    .populate({ path: 'errands' })
     .then((result) => { callback(null, result); })
     .catch((err) => { callback(err, null); });
+};
+
+const getLocation = (filter, callback) => {
+  Location.find(filter)
+    .populate({ path: 'runs' })
+    .populate({ path: 'errands' })
+    .then((result) => { callback(null, result); })
+    .catch((err) => { callback(err, null); });
+};
+
+const getLocationById = (id, callback) => {
+  Location.findById(id)
+    .populate({ path: 'runs' })
+    .populate({ path: 'errands' })
+    .then((result) => { callback(null, result); })
+    .catch((err) => { callback(err, null); });
+};
+
+module.exports = {
+  createLocation,
+  getAllLocations,
+  getLocation,
+  getLocationById,
 };
