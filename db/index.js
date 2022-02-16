5aef0a7cf50e9569afaa22f0accc293767dc3a77
Cameron Colaco <wearenotgood@gmail.com>
2022-02-16 09:51:54 -0800

    Accepting incoming changes for env variables in db/index.js; ran npm install for dotenv.

--- db/index.js
+++ db/index.js
@@ -4,12 +4,7 @@ const { DB_URI } = process.env;
 
 async function main() {
   try {
-<<<<<<< HEAD
-    await mongoose.connect('mongodb://localhost:27017/neighbr');
-    // eslint-disable-next-line no-console
-=======
     await mongoose.connect(DB_URI || 'mongodb://localhost:27017/neighbr');
->>>>>>> master
     console.log('Mongoose connected to Neighbr!');
   } catch (err) {
     // eslint-disable-next-line no-console
