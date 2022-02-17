45148f6399f3f35ef83538fcc4eb74ddc47ef6f4
Yitong Liu <yitong414@gmail.com>
2022-02-16 21:19:06 -0500

    Add authController util

--- controllers/utils.js
+++ controllers/utils.js
@@ -3,9 +3,30 @@ const jsonwebtoken = require('jsonwebtoken');
 
 module.exports = {
   issueJWT: (user) => {
-    // TO DO
+    const { _id } = user;
+    const expiresIn = '1d';
+    const payload = {
+      sub: _id,
+      iat: Date.now()
+    };
+
+    const signedToken = jsonwebtoken.sign(payload, process.env.jwtSecret, { expiresIn, algorithm: 'RS256' });
+    return {
+      token: `Bearer ${signedToken}`,
+      expires: expiresIn
+    };
   },
-  comparePassword: (password) => {
+  comparePassword: (password, hash, salt) => {
+    const hashVerify = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+    return hash === hashVerify;
+  },
+  genPassword: (password) => {
     // TO DO
+    const salt = crypto.randomBytes(32).toString('hex');
+    const genHash = crypto.pbkdf2Sync(password, salt, 10000, 64, 'sha512').toString('hex');
+    return {
+      salt,
+      hash: genHash
+    };
   }
 };
