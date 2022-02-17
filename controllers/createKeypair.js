6234566a6946b9df6cedc843b8589864c4384558
Maxine <maxine.sejin.seo@gmail.com>
2022-02-17 15:53:12 -0800

    added keypair creating function

---
+++ controllers/createKeypair.js
@@ -0,0 +1,20 @@
+const crypto = require('crypto');
+const fs = require('fs');
+
+function genKeyPair() {
+  const keyPair = crypto.generateKeyPairSync('rsa', {
+    modulusLength: 4096,
+    publicKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem'
+    },
+    privateKeyEncoding: {
+      type: 'pkcs1',
+      format: 'pem'
+    }
+  });
+  fs.writeFileSync(`${__dirname}/pub_key.pem`, keyPair.publicKey);
+  fs.writeFileSync(`${__dirname}/priv_key.pem`, keyPair.privateKey);
+}
+
+// genKeyPair();
