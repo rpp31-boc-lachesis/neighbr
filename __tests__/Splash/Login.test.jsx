37e65772ba8b9d20e310f964427a5515be773995
Yitong Liu <yitong414@gmail.com>
2022-02-21 21:31:28 -0500

    Add render test for Signup and login

---
+++ __tests__/Splash/Login.test.jsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import {
+  MemoryRouter
+} from 'react-router-dom';
+import Login from '../../client/src/components/Splash/Login.jsx';
+
+const handleAuth = () => {
+  const nothing = {};
+  return nothing;
+};
+
+const mockedUsedNavigate = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  Navigate: () => mockedUsedNavigate,
+}));
+
+describe('<Login />', () => {
+  it('Renders Login Form component', () => {
+    const user = 'test';
+    const component = render(
+      <MemoryRouter>
+        <Login user={user} handleAuth={handleAuth} />
+      </MemoryRouter>
+    );
+    expect(component).toBeDefined();
+  });
+});
+// npm run test Login.test.jsx
