7d974dafb995ec474393d97305c3aa2fac52a69c
Cameron Colaco <wearenotgood@gmail.com>
2022-02-23 16:33:07 -0800

    Added link in Header for ProfileMain.jsx.

--- client/src/components/Home/Header.jsx
+++ client/src/components/Home/Header.jsx
@@ -121,6 +121,11 @@ export default function Header({ user, userPhoto, logout }) {
                   <Typography sx={{ color: '#EF5DA8' }}>Logout</Typography>
                 </MenuItem>
               </Link>
+              <Link to="/ProfileMain">
+                <MenuItem>
+                  <Typography sx={{ color: '#EF5DA8' }}>Profile</Typography>
+                </MenuItem>
+              </Link>
             </Menu>
           </Box>
         </Box>
