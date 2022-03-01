767f264dbcb8b886b29d243087ae287d9475efad
Cameron Colaco <wearenotgood@gmail.com>
2022-02-28 19:36:44 -0800

    Re-added -w flag to build:dev script in package.json; modified all user services to omit password and salt from their queries; still working on populate query after adding data.

--- db/services/userService.js
+++ db/services/userService.js
@@ -5,20 +5,27 @@ const { Users } = require('../models/index.js');
 
 module.exports = {
   getPopulatedUser: (user) => (
-    Users.findOne({ username: user })
-      .populate('req_history')
+    Users.find({ username: user }, { password: 0, salt: 0 })
+      // .lean()
       .populate('run_history')
+      .populate('req_history')
   ),
   getUsers: (page, count) => {
     const pageNumber = Number(page) === 1 ? 0 : (page * 20 - 20);
     const countNumber = count > 20 ? 20 : count;
-    return Users.find().sort({ _id: 1 }).skip(pageNumber).limit(countNumber);
+    return Users.find(
+      {
+        _id:
+        { $exists: true }
+      },
+      { password: 0, salt: 0 }
+    ).sort({ _id: 1 }).skip(pageNumber).limit(countNumber);
   },
   getOneUser: (user) => (
-    Users.find({ username: user })
+    Users.find({ username: user }, { password: 0, salt: 0 })
   ),
   getUserById: (id) => (
-    Users.find({ _id: id })
+    Users.find({ _id: id }, { password: 0, salt: 0 })
   ),
   addRunToUser: (runId, userId) => {
     Users.findOneAndUpdate(
