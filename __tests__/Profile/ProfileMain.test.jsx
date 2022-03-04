8616d2369bee5ca38af98c8844be2afa96d9634d
Cameron Colaco <wearenotgood@gmail.com>
2022-03-03 16:20:27 -0800

    More tests for ProfileMain.test.jsx.

--- __tests__/Profile/ProfileMain.test.jsx
+++ __tests__/Profile/ProfileMain.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable quotes */
 import React from 'react';
 import 'regenerator-runtime/runtime';
 import {
@@ -77,12 +78,52 @@ describe('<Profile Main />', () => {
   });
   it('Displays membership below User name', async () => {
     const usernameMembership = await screen.findByRole('heading', {
-      name: /Member/i
+      name: /member/i
     });
-    expect(usernameMembership).toHaveTextContent('Member since 2022-02-16');
+    expect(usernameMembership).toHaveTextContent(/member/i);
   });
   it('Displays star rating below membership', async () => {
-    const userStarRating = await screen.findByLabelText('4 Stars');
+    const userStarRating = await screen.findByLabelText(/stars/i);
     expect(userStarRating).toBeInTheDocument();
+  });
+  it(`Shows current user's account number`, async () => {
+    const userAccountNumber = await screen.findByText(/account #/i);
+    expect(userAccountNumber.textContent).toBe('Account #:');
+  });
+  it(`Shows current user's first name`, async () => {
+    const userFirstName = await screen.findByText(/first name/i);
+    expect(userFirstName.textContent).toBe('First Name:');
+  });
+  it(`Shows current user's last name`, async () => {
+    const userFirstName = await screen.findByText(/last name/i);
+    expect(userFirstName.textContent).toBe('Last Name:');
+  });
+  it(`Shows current user's email`, async () => {
+    const userFirstName = await screen.findByText(/email/i);
+    expect(userFirstName.textContent).toBe('Email:');
+  });
+  it(`Shows current user's rating`, async () => {
+    const userFirstName = await screen.findByText(/rating/i);
+    expect(userFirstName.textContent).toBe('Rating:');
+  });
+  it(`Shows current user's biography`, async () => {
+    const userFirstName = await screen.findByText(/bio/i);
+    expect(userFirstName.textContent).toBe('Bio:');
+  });
+  it(`Shows current user's previous runs`, async () => {
+    const runsTab = await screen.findByRole('tab', {
+      name: /runs/i
+    });
+    fireEvent.click(runsTab);
+    const previousRunsText = await screen.findByText(/my run history/i);
+    expect(previousRunsText.textContent).toBe('My Run History:');
+  });
+  it(`Shows location of a user's runs`, async () => {
+    const runsTab = await screen.findByRole('tab', {
+      name: /runs/i
+    });
+    fireEvent.click(runsTab);
+    const runLocationText = await screen.findAllByText(/location/i);
+    expect(runLocationText[0]).toHaveTextContent('Location');
   });
 });
