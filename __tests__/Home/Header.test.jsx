7c3cc3aabd82fbf6082b871dc3becfce2e030e44
Maxine <maxine.sejin.seo@gmail.com>
2022-03-04 17:02:26 -0800

    [WIP] Wring tests

---
+++ __tests__/Home/Header.test.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { Router } from 'react-router-dom';
+
+import Header from '../../client/src/components/Home/Header.jsx';
+
+describe('Header Component', () => {
+  it('Renders upon user signing in / signing up', () => {
+  });
+
+  it('Renders user\'s profile picture', () => {
+
+  });
+
+  it('Links user to main page upon clicking the logo', () => {
+
+  });
+
+  it('Links user to corresponding pages upon clicking menus on the navigation drop-down.', () => {
+    // profilemain
+    // main
+    // runnerDash
+    // runnerList
+  });
+
+  it('Logs out user upon clicking Logout on the menu', () => {
+
+  });
+});
